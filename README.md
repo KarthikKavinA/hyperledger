e40773772f9c1d130779e30972d3c2714f21a2e2
KarthikKavinA <a.kavin24@gmail.com>
2020-03-26 08:50:39 +0530

    Until Key Concepts-Hyperledger Fabric Model Section

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hyperledger
-This repository contains information about the project "Hyperledger Fabric"
+# Hyperledger Fabric
+This repository contains information about the project "Hyperledger Fabric".
+To get deeper Knowledge about Hyperledger Fabric read the file.
--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,14 @@
 #################################################     *ALL TOPICS*     ##################################################
+### Blockchain and its Features ###
+	* decentralized --- replication of ledger across the n/w participants in a blockchain network.
+	* Collaboration --- replication of ledger with, each of whom collaborate in its maintenance.
+	* Systems of Proof --- "immutability" of transactions in ledger makes it simple to determine the provenance of information
+	* Applications --- By means of "smart contracts" which has a business logic. 
+	* Consensus --- process of keeping the ledger transactions synchronized across the network (Achieved by - updating same 			transaction in same order.)
+	* Shared --- a blockchain system has shared programs (smart contracts) to update shared ledgers but in today’s systems, 		     where a participant’s private programs are used to update their private ledgers
+
+
+
 ### Hyperledger Fabric ###
 
 	* open source enterprise-grade permissioned distributed ledger technology (DLT) platform.
@@ -57,12 +67,54 @@
 	* specifies which peer nodes, or how many of them, need to vouch for the correct execution of a given smart contract.
 	* each transaction need only be executed (endorsed) by the subset of the peer nodes necessary to satisfy the transaction’s 		  endorsement policy.
 	* Fabric lifecycle allows you to change an endorsement policy or private data collection configuration without having to 	   repackage or reinstall the chaincode. 
+	
+	
+	
+	
+### Distributed Ledger ###
+	* is a combination of the two components, world state *database* and the transaction log *history*.
+	* 1)world state database
+		* describes the state of the ledger at a given point in time.
+		* it’s the database of the ledger.
+	* 2)transaction log history
+		* records all transactions which have resulted in the current value of the world state.
+		* sequenced, tamper-resistant record of all state transitions in the fabric.
+		* it’s the update history for the world state.
+	* each participant has a copy of the ledger to every Hyperledger Fabric network they belong to.
+	* described as decentralized because it is replicated across many network participants, each of whom collaborate in its 	  maintenance.
+	* by default, this is a LevelDB key-value store database.
+	
+	
+	
+### Features of Fabric Ledger ###
+	* Query and update ledger using key-based lookups, range queries, and composite key queries.
+	* Read-only queries using a rich query language (if using CouchDB as state database).
+	* Read-only history queries — Query ledger history for a key, enabling data provenance scenarios.
+	
+	
+	
+### Transactions ###
+	* consist of the versions of keys/values that were read in chaincode (read set) and keys/values that were written in 		  chaincode (write set).
+	* contain signatures of every endorsing peer and are submitted to ordering service.
+	* are ordered into blocks and are “delivered” from an ordering service to peers on a channel.
+	
+	
+	
+	
+	
+### Peer ###
+	* each peer maintains a copy of the ledger for each channel of which they are a member.
+	* validate transactions against endorsement policies and enforce the policies.
+	* Prior to appending a block (commit to ledger) after validating the transactions, a versioning check is performed to 		  ensure that states for assets that were read have not changed since chaincode execution time. (protection against the 	  double spend)
 		
 		
 		
 ### Smart Contracts ###
 	* business logic of a blockchain application.
 	* functions as a trusted distributed application.
+	* State transitions are a result of chaincode invocations, recorded as transactions.
+	* invoked by an application external to the blockchain when that application needs to interact with the ledger.
+	* chaincode interacts only with the database component of the ledger, the world state (querying it, for example), and not 		  the transaction log.
 	* many smart contracts run concurrently in the network
 	* they may be deployed dynamically (in many cases by anyone)
 	* application code should be treated as untrusted, potentially even malicious.
@@ -74,14 +126,38 @@
 	
 	
 ### Channels ###
-	*  participants on a Fabric network establish a sub-network where every member has visibility to a particular set of 		   transactions.
-	*  only those nodes that participate in a channel have access to the smart contract (chaincode) and data transacted, 		   preserving the privacy and confidentiality of both.
+	* channels keep *transactions* private from the broader network.
+	* participants on a Fabric network establish a sub-network where every member has visibility to a particular set of 		  transactions.
+	* only those nodes that participate in a channel have access to the smart contract (chaincode) and data transacted, 		  preserving the privacy and confidentiality of both.
+	* allowing a group of participants to create a separate ledger of transactions.
+	* One Ledger per Channel.
+	* channel’s ledger contains a configuration block defining policies, access control lists, and other pertinent information.
+	* contain Membership Service Provider instances allowing for crypto materials to be derived from different certificate 		  authorities.
 	
 	
 	
 	
 ### Private Data Collection ###
+	* collections keep *data* private between subsets of organizations on the channel.
+	* is used to segregate the data in a private database, logically separate from the channel ledger, accessible only to the 		  authorized subset of organizations.
 	* It is a collection (like DB) between members on a channel, allowing much of the same protection as channels without the 		  maintenance overhead of creating and maintaining a separate channel.
+	
+	
+	
+	
+### Membership Service Provider MSP ###
+	* members of a Hyperledger Fabric network enroll through a trusted MSP.
+	
+	
+	
+	
+### Assets ###
+	* can be tangible (real estate and hardware)
+	* can also be intangible (contracts and intellectual property)
+	* assets can be modified using chaincode transactions.
+	* represented as a collection of key-value pairs.
+	* with state changes recorded as transactions on a Channel ledger.
+	* can be represented in binary and/or JSON form.
 	
 	
 	
@@ -150,7 +226,21 @@
 ### Confidentiality in Fabric N/W ###
 	* one approach    - Encrypting data -- Given enough time and computational resource, the encryption could be broken.
 	* second approach - Zero knowledge proofs (ZKP) -- computing a ZKP requires considerable time and computational resources. 								   Hence, the trade-off in this case is performance.
+	* Both approaches have their trade-offs. So, Channels and Private Data Collections are used.
+	
+### Encryption of Transactions ###
+	* To further obfuscate the data, values within chaincode can be encrypted (in part or in total) using common cryptographic 		  algorithms such as AES before sending transactions to the ordering service and appending blocks to the ledger. Once 		  encrypted data has been written to the ledger, it can be decrypted only by a user in possession of the corresponding key 		  that was used to generate the cipher text.
+	
+	
+### D/B Channel and Private Data Collections ###
+	* channels keep transactions private from the broader network whereas collections keep data private between subsets of 		  organizations on the channel.
+	
+### Systems of Proof ###
+	* In addition to being decentralized and collaborative, the information recorded to a blockchain is append-only, using 		  cryptographic techniques that guarantee that once a transaction has been added to the ledger it cannot be modified. This 	     property of “immutability” makes it simple to determine the provenance of information because participants can be sure 	   information has not been changed after the fact. It’s why blockchains are sometimes described as systems of proof.
 
+
+### Final Version Check (i.e., Committing phase) - Double Spend Problem ###
+	* final Version Check by peers in committing phase provides protection against *double spend operations* and other threats 		  that might compromise data integrity, and allows for functions to be executed against non-static variables.
 
 
 
@@ -168,6 +258,11 @@
 
 
 #################################################     *Topics*    #################################
+### Importance of Consensus ###
+	* consensus is not merely limited to the agreed upon order of a batch of transactions; rather, it is an overarching 		  characterization that is achieved as a byproduct of the ongoing verifications that take place during a transaction’s 		  journey from proposal to commitment.
+
+
+
 ### Determinism --- Smart Contracts ###
 	* Smart contracts executing in a blockchain that operates with the order-execute architecture must be deterministic; 		  otherwise, consensus might never be reached. To address the non-determinism issue, many platforms require that the smart 		  contracts be written in a non-standard, or domain-specific language (such as Solidity) so that non-deterministic     		  operations can be eliminated. This hinders wide-spread adoption because it requires developers writing smart contracts 		  to learn a new language and may lead to programming errors.
 	
@@ -176,6 +271,17 @@
 ### Decentralized Agreement --- Smart Contracts ###
 	* Human decisions can be modeled into a chaincode process that spans multiple transactions. The chaincode may require 		  actors from various organizations to indicate their terms and conditions of agreement in a ledger transaction. Then, a 		  final chaincode proposal can verify that the conditions from all the individual transactors are met, and “settle” the 	  business transaction with finality across all channel members. For a concrete example of indicating terms and conditions 		  in private, see the asset transfer scenario in the Private data documentation.
 	
+	
+### Permissionless Blockchain ###
+	* open permissionless system that allows unknown identities to participate in the network (requiring protocols like “proof 		  of work” to validate transactions and secure the network), 
+	
+	
+### Consensus - Practical Byzantine Fault Tolerance ###
+	* PBFT can provide a mechanism for file replicas to communicate with each other to keep each copy consistent, even in the 		  event of corruption. Alternatively, in Bitcoin, ordering happens through a process called mining where competing 		  computers race to solve a cryptographic puzzle which defines the order that all processes subsequently build upon.
+	
+	
+### Private Data Collections ###
+	* When a subset of organizations on that channel need to keep their transaction data confidential, a private data 		  collection (collection) is used to segregate this data in a private database, logically separate from the channel 		  ledger, accessible only to the authorized subset of organizations.
 ###################################################################################################	
 
 
